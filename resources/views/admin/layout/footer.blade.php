1a494c24fd1f6fb56fb93db97d492cff157ea6ae
luiscklf <luisandres.klf@gmail.com>
2017-01-18 16:47:54 -0500

    replacing old code with louis code

--- resources/views/admin/layout/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<hr>
-<footer class="text-center">	
-			<em>Copyright &copy; KLF 2017</em>
-</footer>
-
-
--- resources/views/employee/layout/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<hr>
-<footer>
-	<div class="row">
-		<div class="col-sm-5"></div>
-		<div class="col-md-7">
-			<em>Copyright &copy; KLF 2017</em>
-		</div>
-	</div>
-</footer>
