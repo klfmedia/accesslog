78975c7d994528d873f02543ce006ff754ea3aba
luiscklf <luisandres.klf@gmail.com>
2017-01-20 16:01:58 -0500

    Creation of a popup window when the administrator is going to validate the log in order to confirm or deny the log. This window recieves the requiere that the administrator write a message to employee related to the log

--- application/views/pages/requestaccessadmin_view.php
+++ application/views/pages/requestaccessadmin_view.php
@@ -29,12 +29,20 @@
 			<?php echo"<td>".$oneRec['resName']."</td>"?>
 			<?php echo"<td>".$oneRec['reason']."</td>"?>
 			<?php echo"<td>".$oneRec['requestDate']."</td>"?>
-			<?php echo form_open('home/action');?> 
-			<?php echo"<input type='hidden' name='logID' value= '".$oneRec['acclogID']."'/>"?>
-			<?php echo"<td><input type='submit' value='Accept' name='accept'/>&nbsp&nbsp"?>
-			<?php echo"<input type='submit' value='Deny' name='deny'/>"?>
+			
+			
+			<?php echo "<td >" ?>
+			<div style='text-align: center'>
+			<?php $js = 'document.getElementById("id01").style.display="block"'; ?>
+			
+			<?php echo"<button type='button' name='validateButton' onclick='".$js."; myFunction(".$oneRec['acclogID'].");' class='validatebtns'>Validate</button>"?>
+			<?php //echo"<button type='button' name='validateButton' onclick='myFunction(".$oneRec['acclogID'].")' class='validatebtns'>Validate</button>"?>
+			</div>
+			
 			<?php echo"</td>"?>
-			<?php echo form_close();?>
+
+			
+			
 		<?php echo"</tr>"?>
 		
 		<?php } ?>
@@ -42,4 +50,45 @@
 <?php } ?>
 <?php echo $this->pagination->create_links(); ?>
 </div>
+<?php echo form_open('home/action');?> 
+	<?php echo"<input id='logtempID' type='hidden' name='logID'/>"?> 		
+	<div id="id01" class="modal">
+		<div class="modal-content animate">
+			<div class="container">
+				<label><b>Please write a message for your decision</b></label>
+				<textarea class="admincomment" name="reason_comment" placeholder="Please write a comment" cols="100%" rows="10" tabindex="4" required ></textarea>
+      
+			</div>
+			
+			<div class="container btn-accept-div">
+				<input type='submit' value='Accept' name='accept' class='btnaccept'/>
+			</div>
+			<div class="container btn-deny-div">
+				<input type='submit' value='Deny' name='deny' class='btndeny'/>
+			</div>
 
+			<div class="container btn-cancel-div" >
+				<button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+			</div>
+		</div>
+	</div>
+<?php echo form_close();?>
+
+<script>
+function myFunction(myval) {
+	document.getElementById('logtempID').value = myval;
+	//alert(myval);
+	}		
+</script>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
